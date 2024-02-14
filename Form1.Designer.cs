559f70d276575a303a211f5710b302ae6a649cf5
cristi504 <cristian.ionita504@gmail.com>
2024-02-14 22:20:44 +0200

    am incarcat fisierul

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace FallingBall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 8);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Vieti:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 8);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "5";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(75, 7);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Scor:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(117, 7);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 16);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 25.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(363, 285);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(303, 53);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "GAME OVER!";
+            this.label1.Visible = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1056, 566);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Falling Ball";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FallingBall
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        private int padX = 40;
+
+
+        private int ballX = 0;
+        private int ballY = 0;
+
+        private int ballSpeed = 5;
+        private int padSpeed = 20;
+
+
+        private int horizontalWallY = 100;
+        private int verticalWallX = 50;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.Blue, padX, Form1.ActiveForm.Height - 60, 100, 15);
+            e.Graphics.FillEllipse(Brushes.Red, ballX, ballY, 20, 20);
+
+
+            horizontalWallY += 4;
+            verticalWallX += 4;
+
+
+            for (int i = 0; i < Form1.ActiveForm.Width; i += 30)
+            {
+                e.Graphics.FillEllipse(Brushes.Red, i, horizontalWallY, 20, 20);
+            }
+
+            for (int i = 0; i < Form1.ActiveForm.Height; i += 30)
+            {
+                e.Graphics.FillEllipse(Brushes.Red, verticalWallX, i, 20, 20);
+            }
+
+            if (horizontalWallY > Form1.ActiveForm.Height)
+            {
+                horizontalWallY = 0;
+            }
+            if (verticalWallX > Form1.ActiveForm.Width)
+            {
+                verticalWallX = 0;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                padX += padSpeed;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                padX -= padSpeed;
+            }
+            if (e.KeyCode == Keys.Enter)
+            {
+                //  timer1.Start();
+                //label1.Text = "";
+            }
+
+            this.Invalidate();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ballY += ballSpeed;
+
+            // Daca a prins bila
+            if (ballY > Form1.ActiveForm.Height - 60 && ballX > padX && ballX < padX + 100)
+            {
+                int scor = getScor();
+
+                if (scor % 5 == 0 && scor != 0)
+                {
+                    ballSpeed += 4;
+                    padSpeed += 20;
+                }
+                updateScor(scor + 1);
+
+                ballY = 0;
+                ballX = random.Next(0, 500);
+
+            }
+            // Daca nu a prins bila
+            if (ballY > Form1.ActiveForm.Height - 55)
+            {
+                int vieti = int.Parse(label4.Text);
+
+                if (vieti == 1)
+                {
+                    label1.Visible = true;
+                    timer1.Stop();
+                }
+                else
+                {
+                    ballY = 0;
+                    label4.Text = (vieti - 1).ToString();
+                }
+            }
+            this.Invalidate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            timer1.Start();
+            ballX = random.Next(0, 500);
+
+            this.SetStyle(
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.UserPaint |
+                ControlStyles.DoubleBuffer,
+            true);
+        }
+
+        private int getScor()
+        {
+            return int.Parse(label6.Text);
+        }
+
+        private void updateScor(int newScor)
+        {
+            label6.Text = newScor.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+         
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
